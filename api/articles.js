013f9b477171ce53639411e0ce1095b7347e5048
Pascal LEVY <pascallevy@ezekielmsc.home>
2024-10-12 16:24:27 +0200

    database4

--- api/articles.js
+++ api/articles.js
@@ -6,6 +6,7 @@ app.use(express.json());
 
 // Remplacez 'your_mongo_db_uri' par l'URI de connexion à votre base de données
 const mongoURI = 'mongodb+srv://contact:8MtNaCQgwOo5e2Up@wearewe.87p9p.mongodb.net/';
+
 mongoose.connect(mongoURI)
     .then(() => console.log('Connecté à MongoDB...'))
     .catch(err => console.error('Erreur de connexion à MongoDB', err));
