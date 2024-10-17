6c3cb2b63f6a8d7b7f34cf17c5982483b372c965
Pascal LEVY <pascallevy@ezekielmsc.home>
2024-10-17 12:28:28 +0200

    prerender

--- api/robots.js
+++ api/robots.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
     res.setHeader('Content-Type', 'text/plain');
-    res.send(`User-agent: * Disallow: /api/ Allow: / Sitemap: https://www.wearewework.com/sitemap.xml`); 
-
+    res.send(`User-agent: *
+Disallow: /api/
+Allow: /
+Sitemap: https://www.wearewework.com/sitemap.xml`);
 }
-  
