192a94912bddefa9a11d2189febd8ea411ac02e5
Pascal LEVY <pascallevy@ezekielmsc.home>
2024-10-17 11:52:23 +0200

    robots

---
+++ api/robots.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(`User-agent: * Disallow: /api/ Allow: / Sitemap: https://www.wearewework.com/sitemap.xml`); 
+
+}
+  
